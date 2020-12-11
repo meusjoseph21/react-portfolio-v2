f163766e801b8b3cf4e42df5cb4496911b1be984
JosMuse <no-reply>
2020-12-11 11:23:17 -0700

    smooth scroll IMPLEMENTED

---
+++ src/components/NavBar.js
@@ -0,0 +1,43 @@
+import React, {Component} from "react"
+import {Link} from "react-scroll"
+
+
+class NavBar extends Component {
+    state = {}
+    render(){
+        return (
+            <div className="navbar">
+             
+                    <Link
+                    activeClass="active"
+                    to="main"
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={500}>Main
+                    </Link>
+                    <Link
+                    activeClass="active"
+                    to="about"
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={500}>About
+                    </Link>
+                    <Link
+                    activeClass="active"
+                    to="contact"
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={500}>Contact
+                    </Link>
+                    
+                    
+             
+            </div>
+        )
+    }
+}
+
+export default NavBar
---
+++ src/components/section/About.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react"
+
+class About extends Component {
+    state= {}
+    render (){
+        return <div className="about-section" id="about"></div>
+    }
+}
+
+export default About
